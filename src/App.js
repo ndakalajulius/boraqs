82a4e9f018a4f30b30244ac96c6ec5ef5140040e
julius ndakala <juliusndakala35@gmail.com>
2024-01-17 15:21:12 +0300

    stlyed the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <div>
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>What is Lorem Ipsum? </h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </article>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
-function Navbar() {
-    return (
-      <NavbarContainer>
-        <Logo>MyApp</Logo>
-        <NavLinks>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/services">Services</a>
-          <a href="/contact">Contact</a>
-        </NavLinks>
-      </NavbarContainer>
-    );
-  }
-  export default Navbar;
-  
-  const NavbarContainer = styled.nav`
-  background-color: #333;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  padding: 1rem;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
+// Navbar.js
+import "./navbar.css";
+import { useState } from "react";
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        MacroSoft
+      </a>
+      <button className="hamburger"
+      onClick={() => {
+        setIsNavExpanded(!isNavExpanded);
+      }}>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
 
-const Logo = styled.div`
-  font-size: 1.5rem;
-  font-family: 'Sans Serif';
-`;
-
-const NavLinks = styled.div`
-  a {
-    color: white;
-    text-decoration: none;
-    margin-right: 1rem;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`;
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
