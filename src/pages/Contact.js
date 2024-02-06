343c8554bacb4f95687a16bf5696b8590a7a8e6d
julius ndakala <juliusndakala35@gmail.com>
2024-02-06 19:23:14 +0300

    update contact us page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,29 @@
-import React from 'react'
+// ContactPage.js
 
-function Contact() {
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
   return (
-    <div>
-      contact
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <form className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" placeholder="Enter your name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" placeholder="Enter your message"></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
